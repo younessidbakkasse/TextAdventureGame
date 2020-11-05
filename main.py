c292f3039a8e7b243f46b412c29ce909dc504d4c
younessidbakkasse <std.youness@gmail.com>
2020-11-05 11:50:13 +0100

    Initialized the game project with Licence and README

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello world")
+""" This game is made by Youness ID BAKKASSE; https://github.com/younessidbakkasse/
+its a school project to initiat with Python programming 
+for more informations and licence please check README file attached to the folder."""
+
+import pygame, sys
