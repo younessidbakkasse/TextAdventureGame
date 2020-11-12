dd23e43bdc2c8c516cf11a1e68d0209b24c2b4bf
younessidbakkasse <std.youness@gmail.com>
2020-11-12 12:17:45 +0100

    Created a game manager class and started with decoraters

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ for more informations and licence please check README file attached to the folde
 # Importing modules: pygame is a module that allows us to draw on screen
 import pygame, sys
 
-# Game set up
-pygame.init()
-clock = pygame.time.Clock()
-
 # Colors
 color = {
     "black" : (15, 15, 15),
@@ -19,13 +15,17 @@ color = {
 }
 
 class scene:
-    displayWidth = 400
-    displayHeight = 500
-    display = pygame.display.set_mode((displayWidth, displayHeight))
-
     def __init__(self):
         """ Set up the game display """
         self.running, self.click = True, False
+
+    def gameLoop(self, function):
+        """ scene main loop : basicly each scene has its own loop """
+        while self.running:
+            game.display.fill(color["brown"])
+            self.eventLoop()
+            pygame.display.update()
+            game.clock.tick(60)
 
     def eventLoop(self):
         """ This function collects keyboard and mouse clicks from the user """
@@ -39,27 +39,19 @@ class scene:
         gameFont = pygame.font.Font("Minecraft.ttf", size)
         textSurface = gameFont.render(text, False, color)
         textRect = textSurface.get_rect(center = (int(x), int(y)))
-        scene.display.blit(textSurface, textRect)
+        game.display.blit(textSurface, textRect)
 
     def renderImage(self, path, x, y):
         """ Renders images on the screen """
         image = pygame.image.load(path)
         imageRect = image.get_rect(center = (int(x), int(y)))
-        scene.display.blit(image, imageRect)
+        game.display.blit(image, imageRect)
 
-class mainMenuScene(scene):
+class mainMenu(scene):
     """ This is a sub class from scene base class ; sceneMenu = a variant of scene with a menu """
     def __init__(self):
         super().__init__()
-
-    def gameLoop(self):
-        """ scene main loop : basicly each scene has its own loop """
-        while self.running:
-            scene.display.fill(color["brown"])
-            self.render()
-            self.eventLoop()
-            pygame.display.update()
-            clock.tick(60)
+        self.gameLoop(self.render())
 
     def render(self):
         # logo
@@ -67,28 +59,47 @@ class mainMenuScene(scene):
         self.renderText("runner", 200, 100, color["yellow"], 80)
 
         # menu items
-        self.renderText("New game", 200, int(scene.displayHeight/2), color["orange"], 40)
-        self.renderText("Options", 200, int(scene.displayHeight/2) + 50, color["orange"], 40)        
-        self.renderText("Credit", 200, int(scene.displayHeight/2) + 100, color["orange"], 40)
+        self.renderText("New game", 200, int(Game.displayHeight/2), color["orange"], 40)
+        self.renderText("Options", 200, int(Game.displayHeight/2) + 50, color["orange"], 40)        
+        self.renderText("Credit", 200, int(Game.displayHeight/2) + 100, color["orange"], 40)
 
         # render icon
-        self.renderImage("Sound Icon.png", int(scene.displayWidth/2), int(scene.displayHeight - 40))
+        self.renderImage("Sound Icon.png", int(Game.displayWidth/2), int(Game.displayHeight - 40))
 
 
-class MenuScene(scene):
+class Menu(scene):
     """ This is a sub class from scene base class ; sceneMenu = a variant of scene with a menu """
-    def __init__(self):
+    def __init__(self, name):
         super().__init__()
-        self.name = "Credit"
+        self.name = name
 
     def gameLoop(self):
         """ scene main loop : basicly each scene has its own loop """
         while self.running:
-            scene.display.fill(color["brown"])
+            game.display.fill(color["brown"])
             self.renderText(self.name, 200, 50, color["orange"], 40)
             self.eventLoop()
             pygame.display.update()
-            clock.tick(60)
+            game.clock.tick(60)
 
-mainMenu = mainMenuScene()
-mainMenu.gameLoop()
+class Game:
+    displayWidth = 400
+    displayHeight = 500
+    
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.display = pygame.display.set_mode((Game.displayWidth, Game.displayHeight))
+
+    def run(self):
+        self.mainMenu = mainMenu()
+
+
+game = Game()
+game.run()
+
+    
+       
+
+
+
