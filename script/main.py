77dc21717af0025071c024b57f308f585489732d
younessidbakkasse <std.youness@gmail.com>
2020-11-14 19:17:03 +0100

    Added some cmments but still have bitween scene transition bug, it sucks!!

--- script/main.py
+++ script/main.py
@@ -19,17 +19,18 @@ DISPLAY_WIDTH = 400
 
 
 class Scene():
+    # Set up the game display
     pygame.init()
     display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
     clock = pygame.time.Clock()
 
     def __init__(self):
-        """ Set up the game display """
+        """ Constructor method runs when I creat an object using the Scene class """
         self.running= True 
         self.buttons = {}
 
     def run_scene(self):
-        """ scene main loop : basicly each scene has its own loop """
+        """ scene's main loop : basicly each scene has its own loop """
         while self.running:
             Scene.display.fill(colors["brown"])
             self.render_template()
@@ -38,7 +39,7 @@ class Scene():
             Scene.clock.tick(60)
 
     def get_events(self):
-        """ This function collects keyboard and mouse clicks from the user """
+        """ This function collects Mouse clicks from the user """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -58,7 +59,8 @@ class Scene():
 
 
     def render_text(self, text, x, y, color, size = 20):
-        """ Draws any text on the scene window """
+        """ Draws any text on the scene window and also returns a rectangle object wich have 
+        coord attributes and collide methode """
         game_font = pygame.font.Font("./assets/fonts/Minecraft.ttf", size)
         text_surface = game_font.render(text, False, color)
         text_rect = text_surface.get_rect(center = (int(x), int(y)))
@@ -66,7 +68,7 @@ class Scene():
         return text_rect
 
     def render_image(self, path, x, y):
-        """ Renders images on the screen """
+        """ Renders images and icons on the screen and returns a rect object """
         image = pygame.image.load(path)
         image_rect = image.get_rect(center = (int(x), int(y)))
         Scene.display.blit(image, image_rect)
@@ -75,23 +77,23 @@ class Scene():
 
 
 class Menu(Scene):
-    """ This is a sub class from scene base class ; sceneMenu = a variant of scene with a menu """
+    """ This is a sub class from scene base class """
     def __init__(self, name, path):
         super().__init__()
         self.icon = (name, path)
         
     def render_template(self):
         """ This is the template function for scene with similar template """
-        # logo
+        # Logo
         self.render_text("woods", int(DISPLAY_WIDTH/2), 55, colors["yellow"], 32)
         self.render_text("runner", int(DISPLAY_WIDTH/2), 100, colors["yellow"], 80)
 
-        # menu items
+        # Menu items
         self.buttons["game_scene"] = self.render_text("New game", int(DISPLAY_WIDTH/2), int(DISPLAY_HEIGHT/2), colors["orange"], 40)
         self.buttons["option_menu"] = self.render_text("Options", int(DISPLAY_WIDTH/2), int(DISPLAY_HEIGHT/2) + 50, colors["orange"], 40)        
         self.buttons["credit_menu"] = self.render_text("Credit", int(DISPLAY_WIDTH/2), int(DISPLAY_HEIGHT/2) + 100, colors["orange"], 40)
 
-        # render sound icon
+        # Music on/off icon
         self.buttons[self.icon[0]] = self.render_image(self.icon[1], int(DISPLAY_WIDTH/2), int(DISPLAY_HEIGHT - 50))       
 
 class Controle: 
@@ -103,14 +105,12 @@ class Controle:
         # self.scenes["option_menu_description"] = Description("Options")                 
             
     def run_game(self):
-        self.scenes["music_off"].run_scene()
+        self.scenes["music_on"].run_scene()
 
     
 
 controle = Controle()
 controle.run_game()
-controle.scenes["music_on"].transition("music_off")
-
 
     
        
