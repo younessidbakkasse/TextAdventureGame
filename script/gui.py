51423729e9cc2190037358fd1f7b1b0a95c63c28
younessidbakkasse <std.youness@gmail.com>
2020-11-21 22:59:07 +0100

    Instead of creating a whole controle class for storing scene I used a class variable

--- script/gui.py
+++ script/gui.py
@@ -1,26 +1,34 @@
 import pygame, random
 
-# global variables
+# Eng : global variables
+# Fr : 
 DISPLAY_HEIGHT = 500
 DISPLAY_WIDTH = 400
 
 class Gui:
-    """ this class takes care of everything that has to do with rendering content and gui
+    """ Eng : this class takes care of everything that has to do with rendering content and gui
     elements such as buttons, hover effect, text.."""
 
-    # initialising the pygame display and module it's a must
+    """ Fr : this class takes care of everything that has to do with rendering content and gui
+    elements such as buttons, hover effect, text.."""
+
+    # Eng : initialising the pygame display and module it's a must
+    # Fr : 
     pygame.init()
     display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
     clock = pygame.time.Clock()
     pygame.mouse.set_cursor(*pygame.cursors.tri_left)
 
-    # the following loop generates random white squares
+    # Eng : the following loop generates random white squares
+    # Fr :
     stars = [pygame.Rect(random.randint(0, DISPLAY_WIDTH), random.randint(0, DISPLAY_HEIGHT), 3, 3) for i in range(35)]
     
-    # constructed a dict object for gui common button
+    # Eng : constructed a dict object for gui common button
+    # Fr :
     gui_buttons = dict()
 
-    # game color palette
+    # Eng : game color palette
+    # Fr :
     colors = {
         "dark green" : (14, 43, 35),
         "dark blue" : (2, 17, 29),
@@ -31,31 +39,39 @@ class Gui:
     }
 
     def render_ui_buttons(self):
-        """ renders the same user interface buttons acroos all the scenes and stores them on list"""
-        # buttons on the right side
+        """ Eng : renders the same user interface buttons acroos all the scenes and stores them on list"""
+        """ Fr : """
+
+        # Eng : buttons on the right side
+        # Fr :
         self.gui_buttons["scene1"] = self.render_button("button_music_on", DISPLAY_WIDTH - 40, 40)
         self.gui_buttons["scene2"] = self.render_button("button_help", DISPLAY_WIDTH - 90, 40)
 
-        # buttons on the left side
+        # Eng : buttons on the left side
+        # Fr :
         self.gui_buttons["scene3"] = self.render_button("button_return", 40, 40)
         self.gui_buttons["scene4"] = self.render_button("button_pause", 90, 40)
 
-        # buttons on the right down corner 
+        # Eng : buttons on the right down corner 
+        # Fr :
         self.gui_buttons["scene5"] = self.render_button("button_inventory", DISPLAY_WIDTH - 40, DISPLAY_HEIGHT - 40)
         self.gui_buttons["scene6"] = self.render_button("button_character", DISPLAY_WIDTH - 90, DISPLAY_HEIGHT - 40)
 
-        # quest's button on the left down corner
+        # Eng : quest's button on the left down corner
+        # Fr :
         self.gui_buttons["scene7"] = self.render_button("button_large", 85, DISPLAY_HEIGHT - 40)
         self.render_text("quests", 85, DISPLAY_HEIGHT - 39, Gui.colors['white'])
 
     def render_background(self):
-        """ renders a dark green background with random white stars """
+        """ Eng : renders a dark green background with random white stars """
+        """ Fr : """
         Gui.display.fill(Gui.colors["dark green"])
         for star in self.stars:
             pygame.draw.rect(Gui.display, Gui.colors["white"], star)
 
     def render_logo(self, y, big_logo = True):
-        """ renders the two versions of game logo : small & big """
+        """ Eng : renders the two versions of game logo : small & big """
+        """ Fr : """
         if big_logo:
             self.render_text("woods", DISPLAY_WIDTH/2 + 7, y + 7, Gui.colors["dark blue"], 80)
             self.render_text("runner", DISPLAY_WIDTH/2 + 7, y + 67, Gui.colors["dark blue"], 80)
@@ -68,7 +84,8 @@ class Gui:
             self.render_text("runner", DISPLAY_WIDTH/2, y + 22, Gui.colors["white"], 30)
 
     def render_button(self, name, x, y):
-        """ this function draw buttons from the asset/button file and checks hover effect """
+        """ Eng : this function draw buttons from the asset/button file and checks hover effect """
+        """ Fr : """
         path = f"./assets/buttons/buttons_normal/{name}.png"
         button = self.render_image(path, x, y)
         mx, my = pygame.mouse.get_pos()
@@ -78,8 +95,9 @@ class Gui:
             return button_pressed
 
     def render_text(self, text, x, y, color, size = 20):
-        """ Draws any text on the scene window and also returns a rectangle object wich have 
+        """ Eng : Draws any text on the scene window and also returns a rectangle object wich have 
         coord attributes and collide methode """
+        """ Fr : """
         game_font = pygame.font.Font("./assets/fonts/Minecraft.ttf", size)
         text_surface = game_font.render(text, False, color)
         text_rect = text_surface.get_rect(center = (int(x), int(y)))
@@ -87,7 +105,8 @@ class Gui:
         return text_rect
 
     def render_image(self, path, x, y):
-        """ Renders images and icons on the screen and returns a rect object """
+        """ Eng : Renders images and icons on the screen and returns a rect object """
+        """ Fr : """
         image = pygame.image.load(path).convert_alpha()
         image_rect = image.get_rect(center = (int(x), int(y)))
         Gui.display.blit(image, image_rect)
--- script/scene.py
+++ script/scene.py
@@ -1,17 +1,40 @@
-import pygame, sys
+import sys
 from gui import *
 
+# Eng : creating gui object
+# Fr : 
+gui = Gui()
+
 class Scene:
-    # Set up the game display
+    """ Eng : This the Scene Class its a blueprint for every scene for exemple the starting scene
+    has its own loop, its own get event loop, its own template or elements and using this Class 
+    we can create as many scenes as we want """
+
+    """ Fr: """
+
+    scenes = []
+    previous_scene = None
+
     def __init__(self, scene_name):
-        """ Constructor method runs when I creat an object using the Scene class """
-        self.running = True 
+        """ Eng: Constructor method runs every time I create a new scene or page. """
+        """ Fr: Constructor method runs every time I create a new scene or page. """
+
         self.scene_name = scene_name
+        self.running = True 
+
+        """ Eng : every scene has it's own button (besides the gui_buttons) in order to navigate
+        the game story """
+        """ Fr : every scene has it's own button (besides the gui_buttons) in order to navigate
+        the game story """
         self.buttons = {}
+
+        # Eng : mouse coords
+        # Fr : mouse coords
         self.mx, self.my = 0, 0
 
     def run_scene(self):
-        """ scene's main loop : basicly each scene has its own loop """
+        """ Eng : scene's main loop : basicly each scene has its own loop """
+        """ Fr : scene's main loop : basicly each scene has its own loop """
         while self.running:  
             gui.render_background()
             self.render_template()
@@ -20,28 +43,42 @@ class Scene:
             Gui.clock.tick(60)
 
     def render_template(self):
+        """ Eng : """
+        """ Fr : """
         pass
 
     def get_events(self):
-        """ This function collects Mouse clicks from the user """
+        """ Eng : This function collects Mouse clicks (events) from the user """
+        """ Fr : """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                # Eng : to exit the game
+                # Fr : 
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button: 
-                    self.transition(controle.scenes)
+                    self.transition(Scene.scenes)
     
     def transition(self, scenes):
-        """ One hell of an importante method it checks if mouse points to a text or 
+        """ Eng : One hell of an importante method it checks if mouse points to a text or 
         button (rect) and also allows scene transitions from self to another depending 
         on the button's name"""
+        """ Fr : """
         for button_name, button in self.buttons.items():
             self.mx, self.my = pygame.mouse.get_pos()   
             if button != None and button.collidepoint((self.mx, self.my)):
                 for scene_key, scene in scenes.items():
                     if scene_key == button_name:
-                        controle.previous_scene = self.scene_name
+                        Scene.previous_scene = self.scene_name
                         # This may cause bugs in futur if it didn't ur a lucky mothafucka
                         self.buttons.popitem()
                         scene.run_scene()
+
+scene1 = Scene("splash_sceen")
+scene2 = Scene("Home_screen")
+
+Scene.scenes.append(scene1)
+Scene.scenes.append(scene2)
+
+scene2.run_scene()
