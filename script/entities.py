8e5705e5ea7fbee58e3bc85ecae8a6020b8ea6b2
younessidbakkasse <std.youness@gmail.com>
2020-11-29 11:12:38 +0100

    Added a level up system

--- script/entities.py
+++ script/entities.py
@@ -1,6 +1,16 @@
-class Entity:
-    def __init__(self, base_health = 250, level = 1, attack = 10, defence = 2, gold = 10):
+class Level:
+    def __init__(self, level = 0):
         self.level = level
+        self.current_score = 0
+        self.max_score = 5 + (5 * self.level ** 2)
+    
+    
+
+class Entity:
+    def __init__(self, base_health = 250, base_level_score = 10, level = 0, attack = 10, defence = 2, gold = 10):
+        self.level = level
+        self.max_level_score = base_level_score + (base_level_score/2 * self.level)
+        self.current_level_score = level * 10 + (level)
         self.max_health = base_health + (base_health/10 * self.level)
         self.health =  self.max_health
         self.attack = attack * self.level 
