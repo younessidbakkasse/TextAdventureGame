11990c9688c473b92fc1fada1398ec00e9cd4a66
younessidbakkasse <std.youness@gmail.com>
2020-12-06 07:38:17 +0100

    Made changes to __init__

--- script/__init__.py
+++ script/__init__.py
@@ -1,96 +1,100 @@
-def add_time(start, duration, current_day = ''):
-    day_time, day_half = start.split() 
-    start_hour, start_minute = day_time.split(':') 
-    add_hour, add_minute = duration.split(':')
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.initial_balance = 0
+        self.ledger = list()
 
-    days = 0
-    minutes_to_hours = 0
-    week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    current_day_i = None
+    def __str__(self):
+        line = int((30 - len(self.name)) / 2) * '*' + self.name + int((30 - len(self.name)) / 2) * '*'
+        for el in self.ledger:
+            line += '\n' + (el['description'][:23] + (30 - len("{0:.2f}".format(el['amount'])[:7]) - len(el['description'][:23]))  * ' ' + ("{0:.2f}".format(el['amount'])[:7]))
+        line += '\n' + f'Total: {self.get_balance()}'
+        return line
 
-    result_minute = int(start_minute) + int(add_minute)
+    def deposit(self, amount, description = ''):
+        self.initial_balance += amount
+        self.ledger.append({'amount': amount, 'description': description})
 
-    while result_minute >= 60:
-        minutes_to_hours += 1
-        result_minute -= 60
-    
-    result_hour = int(start_hour) + int(add_hour) + minutes_to_hours
-    # days = result_hour // 24
-    # changing houres
-    while result_hour >= 12: 
-        if result_hour >= 12 and day_half == 'AM':
-            day_half = 'PM'
-            if result_hour == 12:
-                break 
-            result_hour -= 12
-        elif result_hour >= 12 and day_half == 'PM':
-            day_half = 'AM'
-            days += 1
-            if result_hour == 12:
-                break 
-            result_hour -= 12 
-    #adding 0 to minutes if there below 10
-    if result_minute < 10:
-        result_minute = '0' + str(result_minute)
-    
-    new_time = f'{result_hour}:{result_minute} {day_half}'
-    if current_day:
-        for i, day in enumerate(week):
-            if day == current_day.capitalize():
-                current_day_i = i
-        if days == 0:
-            new_time += f', {week[current_day_i]}'
-        elif days == 1:
-            new_time += f', {week[current_day_i + 1]} (next day)'
+    def withdraw(self, amount, description = ''):
+        if self.check_funds(amount):
+            self.ledger.append({'amount': - amount, 'description': description})
+            return True
+        return False
+
+    def get_balance(self):
+        balance = 0
+        for op in self.ledger:
+            balance += op['amount']
+        return balance
+
+    def transfer(self, amount, buget_category):
+        if self.check_funds(amount):
+            self.withdraw(amount, description = 'Transfer to ' + buget_category.name)
+            buget_category.deposit(amount, description = 'Transfer from ' + self.name)
+            return True
+        return False
         
-        elif days > 1:
-            print(current_day_i)
-            try:
-                new_time += f', {week[current_day_i + days % 7]} ({days} days later)'
-            except:
-                new_time += f', {week[current_day_i - 1]} ({days} days later)'
-    else:
-        if days == 1:
-            new_time += ' (next day)'
-        elif days > 1:
-            new_time += f' ({days} days later)'
-        
-    print(new_time) 
- 
-add_time("8:16 PM", "466:02", "tuesday")
-# # 6:18 AM, Monday (20 days later)
- 
-# add_time("3:30 PM", "2:12")
-# 5:42 PM
+    def check_funds(self, amount):
+        if amount > self.get_balance():
+            return False
+        return True
 
-# add_time("11:55 AM", "3:12")
-# # 3:07 PM
-
-# add_time("9:15 PM", "5:30")
-# # 2:45 AM (next day)
-
-# add_time("11:40 AM", "0:25")
-# # 12:05 PM
-
-# add_time("2:59 AM", "24:00")
-# # 2:59 AM (next day)
-
-# add_time("11:59 PM", "24:05")
-# # 12:04 AM (2 days later)
-
-# add_time("8:16 PM", "466:02")
-# # 6:18 AM (20 days later)
-
-# add_time("5:01 AM", "0:00")
-# # 5:01 AM
-
-# add_time("3:30 PM", "2:12", "Monday")
-# # 5:42 PM, Monday
-
-# add_time("2:59 AM", "24:00", "saturDay")
-# # 2:59 AM, Sunday (next day)
-
-# add_time("11:59 PM", "24:05", "Wednesday")
-# # 12:04 AM, Friday (2 days later)
+    def get_spent(self):
+        try:
+            spent_percent = 100 - int(100 * self.get_balance()/self.initial_balance)
+        except:
+            return 0
+        if spent_percent % 10 < 5:
+            spent_percent = (spent_percent // 10) * 10
+        else:
+            spent_percent = (spent_percent // 10) * 10 + 10
+        return spent_percent
 
 
+def create_spend_chart(categories):
+    longest_name = ''
+    chart = 'Percentage spent by category'
+    for pourcentage in range(100, -10, -10):
+        chart += '\n' + (3 - len(str(pourcentage))) * ' ' + str(pourcentage) + '|' 
+        for category in categories:
+            if category.get_spent() >= pourcentage:
+                chart += ' o '
+            else:
+                chart += '   '
+    chart += '\n' + 4 * ' '
+    for category in categories:
+        chart += '---'
+        if len(category.name) > len(longest_name):
+            longest_name = category.name
+    chart += '-'
+    for i in range(len(longest_name) + 1):
+        for category in categories:
+            if category.name[i-1:i]:
+                chart += category.name[i-1:i] + 2 * ' '
+            else:
+                chart += 3 * ' '
+        # quick fix
+        if not (i == len(longest_name)):
+            chart += '\n' + 5 * ' '
+    return chart
+
+
+food = Category("Food")
+entertainment = Category("Entertainment")
+business = Category("Business")
+
+food.deposit(900, "deposit")
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
+
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
+
+print(food)
+print(entertainment)
+print(business)
+
+expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+print(create_spend_chart([business, food, entertainment]))
+print(expected)
