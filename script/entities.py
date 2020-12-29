8a2caa0d406a8b907109372d1c76622a5a7788dc
younessidbakkasse <std.youness@gmail.com>
2020-12-29 12:22:36 +0100

    Added loot to the game

--- script/entities.py
+++ script/entities.py
@@ -56,9 +56,59 @@ class Player(Entity):
 
 # loot items
 class Object:
+    items_list = {
+        # Weapons
+        'axe' : {'atk' : 35, 'def' : 5},
+        'bow' : {'atk' : 55, 'def' : 0},
+        'iron sword' : {'atk' : 38, 'def' : 10},
+        'silver sword' : {'atk' : 49, 'def' : 15},
+        'golden sword' : {'atk' : 90, 'def' : 30},
+        'knife' : {'atk' : 12, 'def' : 0},
+        'pickaxe' : {'atk' : 24, 'def' : 3},
+        'hammer' : {'atk' : 30, 'def' : 4},
+        'torch' : {'atk' : 20, 'def' : 30},
+        'wand' : {'atk' : 120, 'def' : 20},
+        'sapphire wand' : {'atk' : 180, 'def' : 20},
+
+        # shields
+        'wooden shield' : {'atk' : 0, 'def' : 40},
+        'iron shield' : {'atk' : 2, 'def' : 60},
+        'wooden armor' : {'atk' : 0, 'def' : 50},
+        'iron armor' : {'atk' : 3, 'def' : 80},
+        'wizard hat' : {'atk' : 15, 'def' : 30},
+
+        # materials
+        'book' : {'name' : 'Book'},
+        'coin' : {'name' : 'Silver Coin'},
+        'key' : {'name' : 'Golden Key'},
+        'map' : {'name' : 'Map'},
+        'gear' :  {'name' : 'Gear'},
+
+        # Monster parts
+        'bone' : {'name' : 'Bone'},
+        'monster meat' : {'name' : 'Monster Meat'},
+        'monster eye' : {'name' : 'Monster Eye'},
+        'skull' : {'name' : 'Human Skull'},
+
+        # Gems
+        'gold ingot' : {'name' : 'Gold Ingot', 'value' : 50},
+        'pearl' : {'name' : 'Pearl', 'value' : 100},
+        'emerald' : {'name' : 'Emerald', 'value' : 150},
+        'topaz' : {'name' : 'Topaz', 'value' : 80},
+        'diamond' : {'name' : 'Diamond', 'value' : 250},
+        'obsidian' : {'name' : 'Obsidian', 'value' : 180},
+
+        # potions
+        'blue potion' : {'name' : 'Blue Potion', 'health' : 100},
+        'green potion' : {'name' : 'Green Potion', 'health' : 150},
+        'red potion' : {'name' : 'Red Potion', 'health' : 200},
+        'water' : {'name' : 'Water', 'health' : 50},
+    }
+
+    
     def __init__(self, name):
         self.name = name
-        self.type = None
+        
         
 
     def __str__(self):
