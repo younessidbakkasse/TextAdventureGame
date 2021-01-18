16f7fe458db3722006232ec400f6fae3f2e3c2f6
younessidbakkasse <std.youness@gmail.com>
2021-01-18 18:38:18 +0100

    Changed README

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ An easy way to create rpg textual games using Python.
 
         pip install textipy
 
-The package will also install pygame 2.0.1
+The commande will also install pygame 2.0.1
 
 
 If you want to install the newest version from git which is compatible with python 3.5 and above and to easily edit the source according to your needs, it's recommended to clone the git
@@ -80,7 +80,7 @@ game.scenes['Second Scene'] = StoryScene(
         name = 'Second Scene',  #the name is required so that you can link scenes with each others.
         buttons = [
                 ['Fight the dragon', 'third scene', 'fight-wild dog'], #first button
-                ['Run away', 'third scene', 'normal'] # second button
+                ['Run away now', 'third scene', 'normal'] # second button
         ],  
         # here third scene is not yet created
         text = "You just met a wild dog do you want to fight him or run away" # the scene text
--- textipy/sample.py
+++ textipy/sample.py
@@ -1,12 +1,19 @@
-from textipy import *       
+from textipy import *            # this will import everything we need from the engine manager with just one line.
 
-
-# always chose 'Pregame' as a name of the first scene
 game.scenes['Pregame'] = StoryScene(
         name = 'Pregame',  #the name is required so that you can link scenes with each others.
-        buttons = [['Start the game', 'second scene', 'normal']],  #the first element is the text on the button
+        buttons = [['Start the game', 'Second Scene', 'normal']],  #the first element is the text on the button
         text = "The front door has been left slightly ajar. Partialy broken, it may have been opened by force." # the scene text
 )
 
+game.scenes['Second Scene'] = StoryScene(
+        name = 'Second Scene',  #the name is required so that you can link scenes with each others.
+        buttons = [
+                ['Fight the dragon', 'third scene', 'fight-wild dog'], #first button
+                ['Run away now', 'third scene', 'normal'] # second button
+        ],  
+        # here third scene is not yet created
+        text = "You just met a wild dog do you want to fight him or run away",# the scene text
+)
 
-game.run()
+game.run() 
